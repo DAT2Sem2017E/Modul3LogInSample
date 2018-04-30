ae0ef7c756ef0b9a87f5bdf6ad0863542d80a471
David-M-Nielsen <wick3dwick@gmail.com>
2018-04-30 10:08:58 +0200

    fixed redirect so it automatically updates.

--- src/main/java/PresentationLayer/UpdateOrderStatus.java
+++ src/main/java/PresentationLayer/UpdateOrderStatus.java
@@ -7,9 +7,15 @@ package PresentationLayer;
 
 import FunctionLayer.LogicFacade;
 import FunctionLayer.LoginSampleException;
+import FunctionLayer.Order;
 import FunctionLayer.requestException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -25,6 +31,11 @@ public class UpdateOrderStatus extends Command
         int currentStatus = Integer.parseInt(request.getParameter("status"));
 
         if (LogicFacade.UpdateOrderStatus(currentStatus + 1, orderId)) {
+            HttpSession session = request.getSession();
+            ArrayList<Order> allOrders = LogicFacade.getOrders();
+
+            session.setAttribute("orderList", allOrders);
+            
             return "employeepage";
         } else {
             return "error";
