0a89bede1fac70ed1682bef3f0020cddb7a49a56
Ra11e <dev.rlumholdt@gmail.com>
2018-05-02 19:01:18 +0200

    added length and height information about sketch

--- src/main/java/FunctionLayer/CarportSideSketcher.java
+++ src/main/java/FunctionLayer/CarportSideSketcher.java
@@ -16,7 +16,8 @@ public class CarportSideSketcher {
      */
     private String buildSvgString(int length, int height, int shedWidth, int shedLength) {
         StringBuilder sb = new StringBuilder();
-        sb.append(buildRoof(length));
+        sb.append(buildArrowheads());
+        sb.append(buildRoof(length, 200));
         sb.append(buildPost(length, 200));
         if(shedWidth > 0 && shedLength >0) {
             sb.append(buildShed(length, 200, shedLength));
@@ -30,12 +31,17 @@ public class CarportSideSketcher {
      * @param length
      * @return svg rectangle string for roof
      */
-    private String buildRoof(int length) {
+    private String buildRoof(int length, int height) {
      
         
         String roof = "<rect x=\"0cm\" y=\"0cm\" width=\""+ length +"cm\" height=\"10cm\" style=\"fill: blue\" />";
         
-        return roof;
+        //Line at the bottom of the carport, to show length
+        String lengthArrow = "<line x1=\"0cm\" y1=\""+(height+50)+"cm\" x2=\""+(length)+"cm\" y2=\""+(height+50)+"cm\" style=\"stroke:#000000\" stroke-width=\"1.5cm\" marker-start=\"url(#beginArrow)\" marker-end=\"url(#endArrow)\"/>";
+        //text that show length of carport
+        String text = "<text x=\""+ (length/2) +"cm\" y=\""+ (height+65) +"cm\" fill=\"red\" font-size=\"10cm\">"+length+" cm.</text>\n";
+        
+        return roof + lengthArrow + text;
     }
 
     /**
@@ -46,9 +52,40 @@ public class CarportSideSketcher {
      */
     private String buildPost(int length, int height) {
         String post1 = "<rect x=\"0cm\" y=\"10cm\" width=\"10cm\" height=\"" + height + "cm\" style=\"fill: black\" />";
-        String post2 = "<rect x=\""+ (length-10) +"cm\" y=\"10cm\" width=\"10cm\" height=\"" + height + "cm\" style=\"fill: blue\" />";
+        String post2 = "<rect x=\""+ (length-10) +"cm\" y=\"10cm\" width=\"10cm\" height=\"" + height + "cm\" style=\"fill: yellow\" />";
+        //String post3 = "<rect x=\""+ (length-30) +"cm\" y=\"10cm\" width=\"10cm\" height=\"" + height + "cm\" style=\"fill: blue\" />";
         
-        String postDefs = "<defs>\n" +
+        //right arrow to show carport height
+        String heightArrow = "<line x1=\""+(length + 10)+"cm\" y1=\"0cm\" x2=\""+(length+10)+"cm\" y2=\""+(height+10)+"cm\" style=\"stroke:#000000\" stroke-width=\"1.5cm\" marker-start=\"url(#beginArrow)\" marker-end=\"url(#endArrow)\"/>";
+        String text = "<text x=\""+ (length+25) +"cm\" y=\""+ (height/2) +"cm\" fill=\"red\" font-size=\"10cm\" >"+height+" cm.</text>\n";
+        
+        return post1 + post2 + heightArrow + text;
+    }
+
+    
+    /**
+     * 
+     * @param length
+     * @param height
+     * @param shedLength
+     * @return String - entire shed + lengths
+     */
+    private String buildShed(int length, int height, int shedLength) {
+        String shed = "<rect x=\""+ (length - shedLength) +"cm\" y=\"10cm\" width=\""+(shedLength-10)+"cm\" height=\"" + height + "cm\" style=\"fill: brown\" />";
+        String door = "<rect x=\""+ (length - shedLength) +"cm\" y=\"40cm\" width=\"4cm\" height=\"" + (height-30) + "cm\" style=\"fill: black\" />";
+        String lengthArrow = "<line x1=\""+ (length - shedLength) +"cm\" y1=\""+(height+20)+"cm\" x2=\""+(length)+"cm\" y2=\""+(height+20)+"cm\" style=\"stroke:#000000\" stroke-width=\"1.5cm\" marker-start=\"url(#beginArrow)\" marker-end=\"url(#endArrow)\"/>";
+        String text = "<text x=\""+ ((shedLength/2)+(length/2) ) +"cm\" y=\""+(height+35)+"cm\" fill=\"red\" font-size=\"10cm\">"+shedLength+" cm.</text>\n";
+        
+        return shed + door + lengthArrow + text;
+    }
+    
+    /**
+     * returns the code to generate the svg arrowheads
+     * @return String
+     */
+    private String buildArrowheads() {
+        //Code for generating the arrowhead on the line that shows length
+        return "<defs>\n" +
             "    <marker id=\"beginArrow\" \n" +
             "    	markerWidth=\"9\" markerHeight=\"9\" \n" +
             "    	refX=\"0\" refY=\"4\" \n" +
@@ -62,16 +99,5 @@ public class CarportSideSketcher {
             "        <path d=\"M0,0 L8,4 L0,8 L0,0\" style=\"fill: #000000;\" />\n" +
             "    </marker>\n" +
             "</defs>";
-        
-        String heightInfo = "<line x1=\""+(length + 10)+"cm\" y1=\"0cm\" x2=\""+(length+10)+"cm\" y2=\""+(height)+"cm\" style=\"stroke:#006600; marker-start: url(#beginArrow); marker-end: url(#endArrow);\"/>";
-        
-        return post1 + post2 + postDefs + heightInfo;
-    }
-
-    private String buildShed(int length, int height, int shedLength) {
-        String shed = "<rect x=\""+ (length - shedLength) +"cm\" y=\"10cm\" width=\""+(shedLength-10)+"cm\" height=\"" + height + "cm\" style=\"fill: brown\" />";
-        String door = "<rect x=\""+ (length - shedLength) +"cm\" y=\"40cm\" width=\"4cm\" height=\"" + (height-30) + "cm\" style=\"fill: black\" />";
-        
-        return shed + door;
     }
 }
